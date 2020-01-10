24933bd4abdcb3417bf90af7ae15ba07e1e1fb30
Olinto <olintojose@gmail.com>
2020-01-10 14:43:27 -0300

    20201001-2

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import React, { Component, Fragment } from 'react';
 import Header from './components/header';
 import ListaNoticias from './components/ListaNoticias';
 
+import Formulario from './components/Formulario';
+
 class App extends Component {
   state={
     noticias:[]
   }
 
   async componentDidMount(){
+    this.consultarNoticias();
+   
+  }
 
-    const url = `https://newsapi.org/v2/top-headlines?country=ve&category=business&apiKey=38707eb90eee45faae2fdf6bcbd204a4`;
+  consultarNoticias = async (categoria='general') => {
+    const url = `https://newsapi.org/v2/top-headlines?country=ve&category=${categoria}&apiKey=38707eb90eee45faae2fdf6bcbd204a4`;
 
     const respuesta = await fetch(url);
     const noticias = await respuesta.json();
@@ -26,8 +32,12 @@ class App extends Component {
         <Header 
         titulo='Noticias API React'
         />
+      
 
         <div className="container white contenedor-noticias">
+        <Formulario 
+        consultarNoticias={this.consultarNoticias}
+        />
           <ListaNoticias 
           noticias={this.state.noticias}
           />
--- src/components/Noticia.js
+++ src/components/Noticia.js
@@ -4,13 +4,23 @@ const Noticia = ({noticia}) => {
 
     // Estraer los datos
     const {urlToImage, url, title, description, source}=noticia;
+
+    // Condicionalmente cargar la imagen si esta dispoible
+
+    const imagen =(urlToImage) ?
+        <div className="card-image">
+            <img src={urlToImage} alt={title}/>
+            <span className="card-title">{source.name}</span>
+        </div>
+    : null;
+
+    
+
     return (
        <div className="col s12 m6 l4">
            <div className="card">
-               <div className="card-image">
-                   <img src={urlToImage} alt={title}/>
 
-               </div>
+                {imagen}
 
                <div className="card-content">
                    <h3>{title}</h3>
